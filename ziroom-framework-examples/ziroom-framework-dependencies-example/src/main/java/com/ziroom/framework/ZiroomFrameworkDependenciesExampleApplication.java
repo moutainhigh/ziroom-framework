54ab3b06028b5f8b01b7ca2713df38d073aa1863
liangrk <liangrk@ziroom.com>
2022-03-15 15:58:29 +0800

    1. 添加ziroom-framework-dependencies-example 2. 删除无用模块

---
+++ ziroom-framework-examples/ziroom-framework-dependencies-example/src/main/java/com/ziroom/framework/ZiroomFrameworkDependenciesExampleApplication.java
@@ -0,0 +1,13 @@
+package com.ziroom.framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZiroomFrameworkDependenciesExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZiroomFrameworkDependenciesExampleApplication.class, args);
+    }
+
+}
---
+++ ziroom-framework-examples/ziroom-framework-dependencies-example/src/test/java/com/ziroom/framework/ZiroomFrameworkDependenciesExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ziroom.framework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZiroomFrameworkDependenciesExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
