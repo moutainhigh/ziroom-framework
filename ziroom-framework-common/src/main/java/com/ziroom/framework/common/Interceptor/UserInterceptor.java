4724035488cd841332df5b63341c3dfe8d640999
kanggh <kanggh@ziroom.com>
2022-03-23 15:43:53 +0800

    ziroom example

---
+++ ziroom-framework-common/src/main/java/com/ziroom/framework/common/Interceptor/UserInterceptor.java
@@ -0,0 +1,28 @@
+package com.ziroom.framework.common.Interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @Description
+ * @Author lidm
+ * @Date 2020/4/20
+ */
+@Slf4j
+public class UserInterceptor implements HandlerInterceptor {
+
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) {
+    }
+}
