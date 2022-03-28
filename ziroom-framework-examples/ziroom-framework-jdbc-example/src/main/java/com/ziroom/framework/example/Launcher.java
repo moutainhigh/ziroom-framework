4c44b90b0522257a786b79eb5d0d3eaafe1d3f8d
kanggh <kanggh@ziroom.com>
2022-03-28 19:47:26 +0800

    jdbc autoconfigure

---
+++ ziroom-framework-examples/ziroom-framework-jdbc-example/src/main/java/com/ziroom/framework/example/Launcher.java
@@ -0,0 +1,16 @@
+package com.ziroom.framework.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by liangrk on 2022/3/18.
+ */
+@SpringBootApplication
+public class Launcher {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Launcher.class, args);
+    }
+
+}
