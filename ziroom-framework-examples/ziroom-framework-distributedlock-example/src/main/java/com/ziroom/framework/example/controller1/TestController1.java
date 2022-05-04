2abe286cbb1d842e864e67e6b241ea0f00bd789d
liangrk <liangrk@ziroom.com>
2022-05-05 04:18:51 +0800

    1 统一异常处理器 2 web日志打印器

---
+++ ziroom-framework-examples/ziroom-framework-distributedlock-example/src/main/java/com/ziroom/framework/example/controller1/TestController1.java
@@ -0,0 +1,20 @@
+package com.ziroom.framework.example.controller1;
+
+import com.ziroom.framework.module.distributedlock.annotation.DistributedLock;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by liangrk on 2022/5/5.
+ */
+@RestController
+public class TestController1 {
+
+    @GetMapping("test1")
+    @DistributedLock(lockName = "ssss")
+    public String test(String ssss) throws InterruptedException {
+        Thread.sleep(5000);
+        return "1";
+    }
+
+}
